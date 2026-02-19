6b09f98a254169908a6af7f379c610d7c5b847da
syedmaaz <maazdeys@gmail.com>
2026-02-19 14:55:35 +0530

    maximum product of subarray

---
+++ maximum-product-of-subarray.py
@@ -0,0 +1,10 @@
+def productofsum(nums):
+    max_prod = min_prod = result = nums[0]
+    for i in range(1, len(nums)):
+        if nums[i] < 0:
+            max_prod, min_prod = min_prod, max_prod
+            max_prod = max(nums[i], max_prod*nums[i])
+            min_prod = max(nums[i], min_prod*nums[i])
+        result = max(result, max_prod)
+        return result
+print(productofsum([-2,3,4,-6]))
